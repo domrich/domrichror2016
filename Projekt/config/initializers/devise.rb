0d169cf94bc3e6e0f58a96fc87e04e66c619f606
domrich <DSRichter@web.de>
2016-09-07 19:22:35 +0200

    mailer fix v3

--- Projekt/config/initializers/devise.rb
+++ Projekt/config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'automaildomrichror@herokuapp.com'
+  config.mailer_sender = 'domrich1810@googlemail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
